fed0932b79ec35717e18026c17c1f8b26403c591
IIZUKARYOGAFB <ryogasapo@gmail.com>
2026-04-02 21:11:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,7 @@
-const CACHE = 'ryoga-fansite-v3';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  // HTMLは常にネットワーク優先
-  if (e.request.destination === 'document') {
-    e.respondWith(
-      fetch(e.request).catch(() => caches.match(e.request))
-    );
-    return;
-  }
-  // それ以外はネットワーク優先・失敗時にキャッシュ
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
 });
